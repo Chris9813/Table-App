e14c475904b0781615b7a67e34df221ee42fc84d
Chris9813 <chordonez@umariana.edu.co>
2022-02-02 17:20:59 -0500

    CRUD echo

--- src/components/screens/DataTable.js
+++ src/components/screens/DataTable.js
@@ -48,14 +48,84 @@ export const Table = () => {
 
   const [data, setData] = useState([]);
   const [modalInst, setModalInst] = useState(false);
+  const [modalEdit, setModalEdit] = useState(false);
+  const [modalDelete, setModalDelete] = useState(false);
+
   const baseurl = "https://jsonplaceholder.typicode.com/posts";
+  const [itemSelect, setItemSelect] = useState({
+    id: 0,
+    title: "",
+    body: "",
+    userId: 0,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setItemSelect((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const openCloseModal = () => {
     setModalInst(!modalInst);
   };
 
+  const openCloseModalEdit = () => {
+    setModalEdit(!modalEdit);
+  };
+
+  const openCloseModalDelete = () => {
+    setModalDelete(!modalDelete);
+  };
+
   const getData = async () => {
     await axios.get(baseurl).then((response) => setData(response.data));
+  };
+
+  const postData = async () => {
+    await axios.post(baseurl, itemSelect).then((response) => {
+      setData(data.concat(response.data));
+      openCloseModal();
+    });
+  };
+
+  const putData = async () => {
+    await axios
+      .put(baseurl + "/" + itemSelect.id, itemSelect)
+      .then((response) => {
+        let dataNew = data;
+        dataNew.map((item) => {
+          if (item.id === itemSelect.id) {
+            item.title = itemSelect.title;
+            item.body = itemSelect.body;
+            item.userId = itemSelect.userId;
+          }
+        });
+        setData(dataNew);
+        openCloseModalEdit();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deleteData = async () => {
+    await axios
+      .delete(baseurl + "/" + itemSelect.id, itemSelect)
+      .then((response) => {
+        setData(data.filter((item) => item.id !== itemSelect.id));
+        openCloseModalDelete();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const selectItem = (item, cas) => {
+    setItemSelect(item)
+      ? cas === "editar" && openCloseModalEdit()
+      : openCloseModalDelete();
   };
 
   useEffect(() => {
@@ -65,25 +135,87 @@ export const Table = () => {
   const bodyInsertar = (
     <div className={styles.modal}>
       <h3>Agregar Nuevo Item</h3>
-      <TextField className={styles.inputMaterial} label="Id" name="id" />
+
       <br />
-      <TextField className={styles.inputMaterial} label="Title" name="title" />
+      <TextField
+        className={styles.inputMaterial}
+        label="Title"
+        name="title"
+        onChange={handleInputChange}
+      />
       <br />
       <TextField
         className={styles.inputMaterial}
         label="Description"
         name="body"
+        onChange={handleInputChange}
       />
       <TextField
         className={styles.inputMaterial}
         label="userId"
         name="userId"
+        onChange={handleInputChange}
       />
       <br />
       <br />
       <div align="right">
-        <Button color="primary">Insertar</Button>
+        <Button color="primary" onClick={() => postData()}>
+          Insertar
+        </Button>
         <Button onClick={() => openCloseModal()}>Cancelar</Button>
+      </div>
+    </div>
+  );
+
+  const bodyEdit = (
+    <div className={styles.modal}>
+      <h3>Edit Item</h3>
+
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        label="Title"
+        name="title"
+        onChange={handleInputChange}
+        value={itemSelect && itemSelect.title}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        label="Description"
+        name="body"
+        onChange={handleInputChange}
+        value={itemSelect && itemSelect.body}
+      />
+      <TextField
+        className={styles.inputMaterial}
+        label="userId"
+        name="userId"
+        onChange={handleInputChange}
+        value={itemSelect && itemSelect.userId}
+      />
+
+      <br />
+      <div align="right">
+        <Button color="primary" onClick={() => putData()}>
+          Editar
+        </Button>
+        <Button onClick={() => openCloseModalEdit()}>Cancelar</Button>
+      </div>
+    </div>
+  );
+
+  const bodyEliminar = (
+    <div className={styles.modal}>
+      <p>
+        Estás seguro que deseas eliminar el item
+        <b>{itemSelect && itemSelect.title}</b>?
+      </p>
+      <div align="right">
+        <Button color="secondary" onClick={() => deleteData()}>
+          Sí
+        </Button>
+        <Button onClick={() => openCloseModalDelete()}>No</Button>
       </div>
     </div>
   );
@@ -92,8 +224,10 @@ export const Table = () => {
 
   return (
     <div style={{ maxWidth: "100%" }}>
+      <button className="btn btn-primary my-3" onClick={() => openCloseModal()}>
+        Insert item
+      </button>
       <hr />
-      <button onClick={() => openCloseModal()}>Insertar item</button>
       <MaterialTable
         data={data}
         columns={columns}
@@ -102,12 +236,12 @@ export const Table = () => {
           {
             icon: "edit",
             tooltip: "Edit Item",
-            onClick: (evento, rowData) => alert("Edit Item" + rowData.title),
+            onClick: (evento, rowData) => selectItem(rowData, "editar"),
           },
           {
             icon: "delete",
             tooltip: "Delete Item",
-            onClick: (evento, rowData) => alert("Delete Item" + rowData.title),
+            onClick: (evento, rowData) => selectItem(rowData, "eliminar"),
           },
         ]}
         options={{
@@ -116,6 +250,14 @@ export const Table = () => {
       />
       <Modal open={modalInst} onClose={openCloseModal}>
         {bodyInsertar}
+      </Modal>
+
+      <Modal open={modalEdit} onClose={openCloseModalEdit}>
+        {bodyEdit}
+      </Modal>
+
+      <Modal open={modalDelete} onClose={openCloseModalDelete}>
+        {bodyEliminar}
       </Modal>
     </div>
   );
